fb3930003b97908de2ce18165474c7f219d8ad8e
antoninosartori <sartoriantonino@gmail.com>
2022-11-30 10:45:07 -0300

    galeria con titulo

--- public/assets/js/carrusel.js
+++ public/assets/js/carrusel.js
@@ -1,32 +1,41 @@
 let contador = 0;
 
-const flechaIzq = document.querySelector('.flechaIzq')
-const flechaDer = document.querySelector('.flechaDer')
-const carruselImg = document.querySelector('.carruselImg')
+const flechaIzq = document.querySelector('.flechaIzq');
+const flechaDer = document.querySelector('.flechaDer');
+const carruselImg = document.querySelector('.carruselImg');
+const carruselImgTitle = document.querySelector('.carruselImg-title');
 
-
-//agregar fotos aca
+//agregar fotos aca => ['url-de-la-foto-', 'titulo-de-la-foto'],
 const galeria = [
-    './assets/img/carrusel/grupocubacultural1.jpg',
-    './assets/img/carrusel/grupoAzc2022.jpg',
-    './assets/img/carrusel/grupoTucumanfolclorico.jpg',
-    './assets/img/carrusel/grupoenGualeguay.jpg',
-    './assets/img/carrusel/grupoenMontevideo.jpg',
+    ['./assets/img/carrusel/grupocubacultural1.jpg','grupo en Cuba'],
+    ['./assets/img/carrusel/grupoAzc2022.jpg','grupo en Azcuenaga'],
+    ['./assets/img/carrusel/grupoTucumanfolclorico.jpg','grupo en Tucuman'],
+    ['./assets/img/carrusel/grupoenGualeguay.jpg','grupo de Gualeguay'],
+    ['./assets/img/carrusel/grupoenMontevideo.jpg','grupo en Montevideo'],
 ];
 
-carruselImg.setAttribute('src', galeria[contador]);
+//iniciando con la img [0]
+carruselImg.setAttribute('src', galeria[contador][0]);
+carruselImg.setAttribute('alt', galeria[contador][1]);
+carruselImgTitle.textContent = galeria[contador][1];
 
 flechaDer.addEventListener('click', () => {
     contador++;
     if(contador > galeria.length - 1){contador = 0};
-    carruselImg.setAttribute('src', galeria[contador]);
+    carruselImg.setAttribute('src', galeria[contador][0]);
+    carruselImg.setAttribute('alt', galeria[contador][1]);
+    carruselImgTitle.textContent = galeria[contador][1];
 });
 
 flechaIzq.addEventListener('click', () => {
     contador--;
     if(contador < 0){contador = galeria.length - 1};
-    carruselImg.setAttribute('src', galeria[contador]);
+    carruselImg.setAttribute('src', galeria[contador][0]);
+    carruselImg.setAttribute('alt', galeria[contador][1]);
+    carruselImgTitle.textContent = galeria[contador][1];
 });
+
+
 
 
 
