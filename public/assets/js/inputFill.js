99f459281952c7c94063ab10214dada6bac0930e
antoninosartori <sartoriantonino@gmail.com>
2022-12-02 21:51:25 -0300

    inputFill function modificada

--- public/assets/js/inputFill.js
+++ public/assets/js/inputFill.js
@@ -5,11 +5,22 @@ const destinoCard1 = document.querySelector('#destino1');
 const destinoCard2 = document.querySelector('#destino2');
 const destinoCard3 = document.querySelector('#destino3');
 
+let destinos = document.querySelectorAll('.destinos');
+
+destinos.forEach(destino => {
+    destino.addEventListener('click', () => {
+        let nombreDestino = destino.querySelector('h4 a').innerHTML;
+
+        fillTextArea(nombreDestino)
+
+    })
+})
+
 function fillTextArea(destino){
     inputTextArea.textContent = `Hola! Quisiera obtener mas informacion sobre el paquete a ${destino}`;
 }
 
-destinoCard1.addEventListener('click', () => {
+/* destinoCard1.addEventListener('click', () => {
     fillTextArea('Roma');
 })
 destinoCard2.addEventListener('click', () => {
@@ -17,4 +28,4 @@ destinoCard2.addEventListener('click', () => {
 })
 destinoCard3.addEventListener('click', () => {
     fillTextArea('Europa completo');
-})
+}) */
