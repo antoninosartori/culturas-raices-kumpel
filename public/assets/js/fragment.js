fbb08e1a3a8a2f26a43e0ddd316c9b217464eae3
antoninosartori <sartoriantonino@gmail.com>
2022-12-11 11:52:54 -0300

    btn filtros de destinos

--- public/assets/js/fragment.js
+++ public/assets/js/fragment.js
@@ -1,51 +1,30 @@
-/* let destinos = [
-    {
-        "id": 0,
-        "title": "Sabor a Colombia",
-        "locations": "Medellin, Bogota, Cartagena",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/View_of_the_Cerro_Morrogacho_in_the_C%C3%B3cora_Valley.jpg/1280px-View_of_the_Cerro_Morrogacho_in_the_C%C3%B3cora_Valley.jpg",
-        "days": 16,
-        "travel_date": ["19 de marzo"]
-    },
-    {
-        "id": 1,
-        "title": "Villa Gesell",
-        "locations": "Buenos Aires",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Villa_Gesell_-_panoramio_-_VANT_Vistas_A%C3%A9reas_%285%29_%28cropped%29.jpg/1920px-Villa_Gesell_-_panoramio_-_VANT_Vistas_A%C3%A9reas_%285%29_%28cropped%29.jpg",
-        "days": 10,
-        "travel_date": ["Enero: 07-12–17–22","6 de Febrero"]
-    },
-    {
-        "id": 2,
-        "title": "Mar del Plata",
-        "locations": "Buenos Aires",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Stensj%C3%B6lejon_i_Mar_del_Plata.jpg/1280px-Stensj%C3%B6lejon_i_Mar_del_Plata.jpg",
-        "days": 10,
-        "travel_date": ["Enero: 04–09–14–19–24","Febrero 03–08–13–21"]
-    },
-    {
-        "id": 3,
-        "title": "Las Grutas",
-        "locations": "Rio Negro",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/M%C3%89DANOS_.jpg/800px-M%C3%89DANOS_.jpg",
-        "days": 10,
-        "travel_date": ["9 - 23 de Enero","6 de Febrero"]
-    }
-];
- */
 const destinosDiv = document.querySelector('#destinosContainer');
 const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
+//btn
+const btnNacional = document.querySelector('.btnNacional');
+const btnInternacional = document.querySelector('.btnInternacional');
 
 async function getDestinos() {
     const dataFetch = await fetch('./public/assets/js/destinos.json');
     const response = await dataFetch.json();
 
     pintarCardsDestinos(response)
+    return destinosArray = response
 }
 
-const pintarCardsDestinos = destinos => {
+btnNacional.addEventListener('click', () => {
+    let isNacional = destinosArray.filter(destino => destino.isNacional);
+    pintarCardsDestinos(isNacional)
+})
 
+btnInternacional.addEventListener('click', () => {
+    let isInternacional = destinosArray.filter(destino => !destino.isNacional);
+    pintarCardsDestinos(isInternacional)
+})
+
+const pintarCardsDestinos = destinos => {
+    destinosDiv.textContent = '';
     let destinoLength = destinos.length
     destinos.map(destino => {
         
@@ -62,7 +41,6 @@ const pintarCardsDestinos = destinos => {
         const divDateTravel = templateCard.querySelector('.card-date');
         divDateTravel.textContent = '';
         destino.travel_date.map(date => {
-            //  templateCard.querySelector('.card-date').textContent = date
             const spanDateTravel = document.createElement('span');
             spanDateTravel.classList.add('fs-0', 'fw-medium');
             spanDateTravel.textContent = `${date}`;
